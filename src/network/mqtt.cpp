84020462795d93d7499a29f92d4471e475d64142
Doogle007 <1961590411@qq.com>
2026-03-19 13:21:15 +0800

    1

--- src/network/mqtt.cpp
+++ src/network/mqtt.cpp
@@ -4,8 +4,12 @@
 #include "network/mqtt.h"
 
 void mqttCallback(char*,byte*,unsigned int);
+void helloPub(void);
+void dataPub(void);
+void controlSub(void);
 
 static char mqtt_topic_data_pub_buf[128] = {0};
+static char mqtt_topic_hello_pub_buf[128] = {0};
 static char mqtt_topic_control_sub_buf[128] = {0};
 
 /** MQTT相关配置信息 */
@@ -16,7 +20,9 @@ const  char    *mqtt_username              = nullptr;                           
 static char     mqtt_client_id[13]         = {0};                                  //客户端ID
 const  char    *mqtt_password              = "IBAS";                               //密码
 
-const  char    *mqtt_topic_hello_pub       = "IBAS/system/device/hello";           //发布主题Hello
+const  char    *mqtt_topic_pre_hello_pub   = "IBAS/system/device/hello/";          //发布主题Hello
+const  char    *mqtt_topic_hello_pub       = mqtt_topic_hello_pub_buf;             //发布主题Data
+
 const  char    *mqtt_topic_pre_data_pub    = "IBAS/system/device/group/";          //发布主题Data前缀
 const  char    *mqtt_topic_data_pub        = mqtt_topic_data_pub_buf;              //发布主题Data
 
@@ -27,12 +33,20 @@ WiFiClient tcpClient;
 PubSubClient mqttClient;
 //声明一个客户端对象，用于与服务器进行连接
 
+unsigned long previousConnectMillis       = 0;        // 毫秒时间记录
+const long    intervalConnectMillis       = 5000;     // 时间间隔
+unsigned long previousHelloPublishMillis  = 0;        // 毫秒时间记录
+const long    intervalHelloPublishMillis  = 3600000;  // 时间间隔
+unsigned long previousDataPublishMillis   = 0;        // 毫秒时间记录
+const long    intervalDataPublishMillis   = 60000;    // 时间间隔
+
 void initMQTT(void){
   mqtt_username = FlashData.mqttName;
   uint8_t mac[6];
   WiFi.macAddress(mac);
   snprintf(mqtt_client_id, 13,"%02X%02X%02X%02X%02X%02X", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
 
+  snprintf(mqtt_topic_hello_pub_buf, sizeof(mqtt_topic_hello_pub_buf), "%s%s", mqtt_topic_pre_hello_pub, mqtt_client_id);
   snprintf(mqtt_topic_data_pub_buf, sizeof(mqtt_topic_data_pub_buf), "%s%s", mqtt_topic_pre_data_pub, FlashData.group);
   snprintf(mqtt_topic_control_sub_buf, sizeof(mqtt_topic_control_sub_buf), "%s%s", mqtt_topic_pre_control_sub, mqtt_client_id);
 
@@ -42,8 +56,9 @@ void initMQTT(void){
 }
 
 void mqttCallback(char *topic, byte *payload, unsigned int length){
-  boolean needSaveFlash = false;
+  boolean needSaveFlash   = false;
   boolean needDataRefresh = false;
+  boolean needDisconnect  = false;
   JsonDocument jsonBuffer;
   DeserializationError err = deserializeJson(jsonBuffer, payload, length);
   if(err){
@@ -56,29 +71,40 @@ void mqttCallback(char *topic, byte *payload, unsigned int length){
   Serial.print("收到订阅的json: ");
   Serial.println(input);
   if(!jsonBuffer["params"]["AlarmManual"].isNull()){
-    set_alarm_manual(jsonBuffer["params"]["AlarmManual"]);
+    set_alarm_manual(jsonBuffer["params"]["AlarmManual"].as<bool>());
     needDataRefresh = true;
   }
   if(!jsonBuffer["params"]["AlarmAuto"].isNull()){
-    set_alarm_auto(jsonBuffer["params"]["AlarmAuto"]);
+    set_alarm_auto(jsonBuffer["params"]["AlarmAuto"].as<bool>());
     needDataRefresh = true;
   }
   if(!jsonBuffer["params"]["Group"].isNull()) {
-    strncpy(FlashData.group, jsonBuffer["params"]["Group"], 31);
-    FlashData.group[31] = '\0';
-    FlashData.hasGroup = true;
-    needSaveFlash = true;
-    snprintf(mqtt_topic_data_pub_buf, sizeof(mqtt_topic_data_pub_buf), "%s%s", mqtt_topic_pre_data_pub, FlashData.group);
+    const char* newGroup = jsonBuffer["params"]["Group"].as<const char*>();
+    if (newGroup && strncmp(FlashData.group, newGroup, sizeof(FlashData.group)) != 0) {
+      strncpy(FlashData.group, newGroup, sizeof(FlashData.group) - 1);
+      FlashData.group[sizeof(FlashData.group) - 1] = '\0';
+      FlashData.hasGroup = (FlashData.group[0] != '\0');
+      needSaveFlash = true;
+      snprintf(mqtt_topic_data_pub_buf, sizeof(mqtt_topic_data_pub_buf), "%s%s", mqtt_topic_pre_data_pub, FlashData.group);
+    }
   }
   if(!jsonBuffer["params"]["Name"].isNull()) {
-    strncpy(FlashData.mqttName, jsonBuffer["params"]["Name"], 31);
-    FlashData.mqttName[31] = '\0';
-    needSaveFlash = true;
+    const char* newName = jsonBuffer["params"]["Name"].as<const char*>();
+    if (newName && strncmp(FlashData.mqttName, newName, sizeof(FlashData.mqttName)) != 0) {
+      strncpy(FlashData.mqttName, newName, sizeof(FlashData.mqttName) - 1);
+      FlashData.mqttName[sizeof(FlashData.mqttName) - 1] = '\0';
+      mqtt_username = FlashData.mqttName;
+      needSaveFlash = true;
+      needDisconnect = true;
+    }
   }
   if(!jsonBuffer["params"]["LogicalAddress"].isNull()) {
-    strncpy(FlashData.logicalAddress, jsonBuffer["params"]["LogicalAddress"], 63);
-    FlashData.logicalAddress[63] = '\0';
-    needSaveFlash = true;
+    const char* newLogicalAddress = jsonBuffer["params"]["LogicalAddress"].as<const char*>();
+    if (newLogicalAddress && strncmp(FlashData.logicalAddress, newLogicalAddress, sizeof(FlashData.logicalAddress)) != 0) {
+      strncpy(FlashData.logicalAddress, newLogicalAddress, sizeof(FlashData.logicalAddress) - 1);
+      FlashData.logicalAddress[sizeof(FlashData.logicalAddress) - 1] = '\0';
+      needSaveFlash = true;
+    }
   }
   if(needSaveFlash){
     saveFlash();
@@ -86,14 +112,14 @@ void mqttCallback(char *topic, byte *payload, unsigned int length){
   }
   if(needDataRefresh)
     dataRefresh();
+  if(needDisconnect){
+    previousConnectMillis = 0;
+    dataRefresh();
+    helloRefresh();
+    mqttClient.disconnect();
+    return;
+  }
 }
-
-unsigned long previousConnectMillis = 0; // 毫秒时间记录
-const long intervalConnectMillis = 5000; // 时间间隔
-unsigned long previousHelloPublishMillis = 0; // 毫秒时间记录
-const long intervalHelloPublishMillis = 3600000; // 时间间隔
-unsigned long previousDataPublishMillis = 0; // 毫秒时间记录
-const long intervalDataPublishMillis = 60000; // 时间间隔
 
 void loopMQTT(void){
   /** 查询连接状态 */
@@ -111,7 +137,7 @@ void loopMQTT(void){
         Serial.println(mqttClient.state()); // 连接成功后可以订阅主题
       }
       else{
-        Serial.print("MQTT连接成功!");
+        Serial.println("MQTT连接成功!");
         controlSub();
       }
     }
@@ -153,11 +179,15 @@ void helloPub(void){
 }
 
 void dataPub(void){
+  if(!FlashData.hasGroup){
+    Serial.println("设备未分组，本次发布跳过！");
+    return;
+  }
   JsonDocument jsonBuffer;
   JsonObject root = jsonBuffer["params"].to<JsonObject>();
   root["Name"] = FlashData.mqttName;
-  root["AlarmManual"] = get_alarm_manual() ? "true" : "false";
-  root["AlarmAuto"] = get_alarm_auto() ? "true" : "false";
+  root["AlarmManual"] = get_alarm_manual();
+  root["AlarmAuto"]   = get_alarm_auto();
   String output;
   serializeJson(jsonBuffer, output);
   Serial.print("即将推送的json: ");
